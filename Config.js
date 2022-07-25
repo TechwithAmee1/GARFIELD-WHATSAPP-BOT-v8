181ddb6d0892815178c624fc76631e9757688971
TechwithAmee1 <107279170+TechwithAmee1@users.noreply.github.com>
2022-07-25 17:22:15 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -13,7 +13,7 @@ global.APIKeys = {
 	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 // 👈 You Can change this your choice
-global.emoji01 = ['🐼'] // 👈 Enter your Emoji 
+global.emoji01 = ['😈'] // 👈 Enter your Emoji 
 global.emoji02 = ['🦋'] //👈 Enter your Emoji
 global.notice = ` *Hi 🥰 I am*
 *៚ᴍʀͥ.ᴀͣᴍͫɪʏᴀ͢⁸⁵⁴¹⃟✦⃝̮̮〽️ Ξ*
